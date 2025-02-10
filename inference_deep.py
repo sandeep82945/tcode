4e713b50eb0f1e9db4cb30d0e977f4094fbfc096
sane <harry@pp>
2025-02-10 20:00:14 +0530

    file added

--- inference_deep.py
+++ inference_deep.py
@@ -13,13 +13,27 @@ load_in_4bit = True
 
 fine_tuned_model_dir = "/scratch/ttc/sandeep/hypothesis_deep"
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = fine_tuned_model_dir,
+    model_name = "unsloth/DeepSeek-R1-Distill-Llama-8B",
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = load_in_4bit,
+    token = "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",
 )
 
+model = FastLanguageModel.get_peft_model(
+    model,
+    r=16,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+    lora_alpha=16,
+    lora_dropout=0,
+    bias="none",
+    use_gradient_checkpointing="unsloth",
+    random_state=3407,
+    use_rslora=False,
+    loftq_config=None,
+)
 
+model = model.merge_and_unload()
 model.eval()
 
 # Function to generate response
